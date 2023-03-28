00f0ce15ab44fa9b82fad200b16d551d29297b81
anmolpurba <92576576+anmolpurba@users.noreply.github.com>
2023-03-28 16:45:35 +0530

    writing done

--- src/components/dashboardIelts/Writing/Writing1.js
+++ src/components/dashboardIelts/Writing/Writing1.js
@@ -1,13 +1,17 @@
 import {React,useEffect,useState} from 'react'
 import Axios from 'axios'
 import "./Writing1.css"
+import ReactCountdownClock from "react-countdown-clock"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Writing1() {
 
     var link = document.location.href.split('/')[4];
     const [writing,Setwriting] = useState("");
     const [wordcount,Setwordcount] = useState(0)
-
+    const [showclock1,Setshowclock1] = useState(true);
+    const [showclock2,Setshowclock2] = useState(true);
 
 
     useEffect(()=>{
@@ -15,7 +19,7 @@ function Writing1() {
         Axios.get(`http://localhost:1337/api/writings/${link}/?populate=*`)
         .then(response => {
             Setwriting(response.data.data.attributes);
-            console.log(response.data.data.attributes);
+            // console.log(response.data.data.attributes);
         })
         .catch(error => {
             console.log('An error occurred:', error.response);
@@ -23,8 +27,44 @@ function Writing1() {
 
     },[])
 
+    function submitTask(){
+        Axios.post("http://localhost:1337/api/writing-answers", {
+                data: {
+                studentEmail:document.getElementById("studentEmail").value,
+                task1:document.getElementById("task1").value,
+                task2:document.getElementById("task2").value,
+                },
+            })
+            .then((response) => {
+                toast.success('Your Answers are submitted for review 😃 ', {
+                    position: "top-center",
+                    autoClose: 1500,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                    });
+                console.log(response);
+            }).catch(error=>{
+                toast.error('🦄 Something Went Wrong', {
+                    position: "top-center",
+                    autoClose: 1500,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                });
+                console.log('An error occurred44:', error.response)
+            });
+    }
+
   return (
     <>
+    <ToastContainer />
     <div className='row'>
         <div className="col" style={{padding:"5rem"}}>
             <h2 style={{fontSize: "32px",fontWeight:"bold",color: "#faaa5a"}}>Writing Task 1</h2>
@@ -32,13 +72,22 @@ function Writing1() {
             <p class="parawriting">{writing.questionText1}</p>
             <p class="parawriting">You should write <strong>at least 150 words.</strong></p>
             <img src={writing && `http://localhost:1337${writing.diagramImage.data[0].attributes.formats.medium.url}`} alt="" />
+            <ReactCountdownClock seconds={1200}
+                     color="#faaa5a"
+                     alpha={0.9}
+                     size={120}
+                     paused={showclock1}
+
+            />
+            
         </div>
 
 
         <div className="col" style={{backgroundColor:"#feeede",padding:"5rem"}}>
             <h3 className='parawriting1' >Type your essay below and click Submit for evaluation</h3>
+            <p className='parawriting' style={{color:"red"}} >* Timer will start once you start typing in text area.</p>
             <div className="textArea">
-                <textarea onChange={(e)=>{Setwordcount(e.target.value.split(" ").filter((temp)=>{return temp!=""}).length)}} className='textAreaAnswer'></textarea>
+                <textarea id='task1' onClick={()=>{Setshowclock1(false)}} onChange={(e)=>{Setwordcount(e.target.value.split(" ").filter((temp)=>{return temp!=""}).length)}} className='textAreaAnswer'></textarea>
             </div>
             <p className='parawriting'>Words: <span className='parawriting1'>{wordcount}</span></p>
         </div>
@@ -54,15 +103,25 @@ function Writing1() {
             <p class="parawriting">You should spend about <strong>40 minutes</strong> on this task.</p>
             <p class="parawriting">{writing.questionText2}</p>
             <p class="parawriting">You should write <strong>at least 254 words.</strong></p>
+            <ReactCountdownClock seconds={2400}
+                     color="#faaa5a"
+                     alpha={0.9}
+                     size={120}
+                     paused={showclock2}
+
+            />
         </div>
 
 
         <div className="col" style={{backgroundColor:"#feeede",padding:"5rem"}}>
             <h3 className='parawriting1' >Type your essay below and click Submit for evaluation</h3>
+            <p className='parawriting' style={{color:"red"}} >Timer will start once you start typing in text area.</p>
             <div className="textArea">
-                <textarea onChange={(e)=>{Setwordcount(e.target.value.split(" ").filter((temp)=>{return temp!=""}).length)}} className='textAreaAnswer'></textarea>
+                <textarea id='task2' onClick={()=>{Setshowclock2(false)}} onChange={(e)=>{Setwordcount(e.target.value.split(" ").filter((temp)=>{return temp!=""}).length)}} className='textAreaAnswer'></textarea>
             </div>
             <p className='parawriting'>Words: <span className='parawriting1'>{wordcount}</span></p>
+            <input id="studentEmail" style={{border:"none",borderBottom: "2px solid #aaa",marginRight:"1rem",backgroundColor:"#feeede"}} type="text" placeholder='Enter Your Email' />
+            <button onClick={submitTask} style={{color:"white",backgroundColor:"#284664"}} type="button" class="btn btn-secondary"> <span><i class="fa-solid fa-paper-plane"></i></span> Submit the Test</button>
         </div>
     </div>
     </>
