5cfdf1f43362d9ab7735276639469708a5903c4f
anmolpurba <92576576+anmolpurba@users.noreply.github.com>
2023-03-29 18:18:16 +0530

    npm react-calender added

--- src/components/dashboardIelts/Speaking/Dashspeaking.js
+++ src/components/dashboardIelts/Speaking/Dashspeaking.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import {React,useState} from 'react'
 import DashboardNav from '../../dashboardNavbar/DashboardNav'
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
+
 
 
 function Dashspeaking() {
+  const [value, onChange] = useState(new Date());
   return (
     <div>
     <DashboardNav />
-    Dashspeaking
+      <div className="calendar" style={{margin:"2rem"}}>
+        <Calendar onClickDay={(event)=>{console.log(event)}} onChange={onChange} value={value} />
+      </div>
     </div>
   )
 }
