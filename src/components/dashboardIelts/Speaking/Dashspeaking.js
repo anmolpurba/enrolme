10e91161f6e6eafbf6c4e1c4cba5be73ec0ee858
anmolpurba <92576576+anmolpurba@users.noreply.github.com>
2023-03-30 14:56:32 +0530

    calendly added in speaking section

--- src/components/dashboardIelts/Speaking/Dashspeaking.js
+++ src/components/dashboardIelts/Speaking/Dashspeaking.js
@@ -1,17 +1,19 @@
 import {React,useState} from 'react'
 import DashboardNav from '../../dashboardNavbar/DashboardNav'
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-
-
+import { InlineWidget } from "react-calendly";
 
 function Dashspeaking() {
   const [value, onChange] = useState(new Date());
   return (
     <div>
     <DashboardNav />
-      <div className="calendar" style={{margin:"2rem"}}>
+      {/* <div className="calendar" style={{margin:"2rem"}}>
         <Calendar onClickDay={(event)=>{console.log(event)}} onChange={onChange} value={value} />
+      </div> */}
+
+      <div className="calendly">
+        <InlineWidget url="https://calendly.com/enrolmespeaking" />
+
       </div>
     </div>
   )
